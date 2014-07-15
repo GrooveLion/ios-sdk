ab9d9ce1f250b001a55edd68f4e6a5fd07cdf010
Philip Willoughby <philip.willoughby@dotankstudios.com>
2014-07-15 14:50:42 +0100

    AppleDoc comments

--- SDK/GrooveLion.h
+++ SDK/GrooveLion.h
@@ -4,6 +4,7 @@
 
 /** This class represents a segment (also known as a Tag Group)
  
+ GLEMutableSegment is the mutable subclass of this class
  */
 @interface GLESegment : NSObject <NSCopying,NSMutableCopying>
 
@@ -13,9 +14,9 @@
  @param tags an array of strings representing the application-defined tags within that segment
  */
 -(instancetype)initWithName:(NSString *)name tags:(NSArray *)tags;
-/// Application-defined segment name
+/** Application-defined segment name */
 @property(readonly,nonatomic,copy)NSString *name;
-/// An array of strings representing the application-defined tags within this segment
+/** An array of strings representing the application-defined tags within this segment */
 @property(readonly,nonatomic,copy)NSArray *tags;
 
 @end
@@ -25,9 +26,9 @@
  */
 @interface GLEMutableSegment : GLESegment
 
-/// Application-defined segment name
+/** Application-defined segment name */
 @property(readwrite,nonatomic,copy)NSString *name;
-/// An array of strings representing the application-defined tags within this segment
+/** An array of strings representing the application-defined tags within this segment */
 @property(readwrite,nonatomic,copy)NSArray *tags;
 
 @end
@@ -37,12 +38,12 @@
  */
 @interface GLECentral : NSObject <CLLocationManagerDelegate>
 
+/** @name Debugging */
 /** Query whether debug logging is enabled
  
  The default is YES iff the DEBUG preprocessor macro is defined true at compile time.
  
  @return YES iff debug logging is enabled
- @name Debugging
  */
 +(BOOL)debugLoggingEnabled;
 
@@ -51,20 +52,20 @@
  The default is YES iff the DEBUG preprocessor macro is defined true at compile time.
  
  @param enabled YES iff debug logging is required
- @name Debugging
  */
 +(void)setDebugLoggingEnabled:(BOOL)enabled;
 
+/** @name Identifying the Customer */
 /** Application-defined identifier for the customer whose device this is.
  */
 @property(readwrite,nonatomic,copy)NSString *customerId;
 
+/** @name Getting an instance */
 /** Access the shared GLECentral instance
  
 Iff the first call is to sharedGrooveLionWithServerURL the server URL of the shared instance can be overridden from the default. The default is suitable for customers using the cloud service at https://engage.groovelion.com
  
  @return the shared GLECentral instance
- @name Getting an instance
  */
 +(instancetype)sharedGrooveLion;
 
@@ -74,85 +75,133 @@ Iff the first call is to sharedGrooveLionWithServerURL the server URL of the sha
  
  @param url the URL of the Groove Lion installation
  @return the shared GLECentral instance
- @name Getting an instance
  */
 +(instancetype)sharedGrooveLionWithServerURL:(NSURL *)url;
 
-/// @name Pass-through methods from the application delegate
+/** @name Pass-through methods from the application delegate */
+/** Refer to the documentation for the UIApplicationDelegate protocol
+ 
+ @param application the shared UIApplication instance
+ @param deviceToken the device token given by the system
+ */
 - (void)application:(UIApplication *)application didRegisterForRemoteNotificationsWithDeviceToken:(NSData *)deviceToken;
-/// @name Pass-through methods from the application delegate
+/** Refer to the documentation for the UIApplicationDelegate protocol
+ 
+ @param application the shared UIApplication instance
+ @param error the error given by the system
+ */
 - (void)application:(UIApplication *)application didFailToRegisterForRemoteNotificationsWithError:(NSError *)error;
-/// @name Pass-through methods from the application delegate
+/** Refer to the documentation for the UIApplicationDelegate protocol
+ 
+ @param application the shared UIApplication instance
+ @param launchOptions the launch options given by the system
+ @return YES
+ */
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions;
-/// @name Pass-through methods from the application delegate
+/** Refer to the documentation for the UIApplicationDelegate protocol
+ 
+ @param application the shared UIApplication instance
+ @param userInfo the user info given by the system
+ */
 - (void)application:(UIApplication *)application didReceiveRemoteNotification:(NSDictionary *)userInfo;
-/// @name Pass-through methods from the application delegate
+/** Refer to the documentation for the UIApplicationDelegate protocol
+ 
+ @param application the shared UIApplication instance
+ @param userInfo the user info given by the system
+ @param handler the handler given by the system
+ */
 - (void)application:(UIApplication *)application didReceiveRemoteNotification:(NSDictionary *)userInfo fetchCompletionHandler:(void (^)(UIBackgroundFetchResult result))handler;
 
 
-/// @name Raw segment-manipulation methods
+/** @name Raw segment-manipulation methods */
+/** Install (or replace) a single segment subscription
+ 
+ This replaces any existing segment with the same name
+ 
+ @warning Groove Lion recommends that you use the methods in GLESegmentTransaction instead of this method.
+ @param segment a segment subscription to replace any existing segment subscription with the same name */
 -(void)setSegmentSubscription:(GLESegment *)segment;
-/// @name Raw segment-manipulation methods
+/** Retrieve a single segment subscription
+ 
+ @warning Groove Lion recommends that you use the methods in GLESegmentTransaction instead of this method.
+ @param segmentName the name of the segment to find
+ @return the current segment subscription for that segment name */
 -(GLESegment *)segmentSubscriptionForName:(NSString *)segmentName;
-/// @name Raw segment-manipulation methods
+/** Install (or replace) all segment subscriptions
+ 
+ Any existing segments are removed and replaced by the contents of the segments array
+ 
+ @warning Groove Lion recommends that you use the methods in GLESegmentTransaction instead of this method.
+ @param segments Array of GLESegment objects which replace all existing segment subscriptions */
 -(void)setSegmentSubscriptions:(NSArray *)segments;
-/// @name Raw segment-manipulation methods
+/** Retrieve all segment subscriptions
+ 
+ @warning Groove Lion recommends that you use the methods in GLESegmentTransaction instead of this method.
+ @return Array of GLESegment objects representing all existing segment subscriptions */
 -(NSArray *)segmentSubscriptions;
 
-/// @name Pass-through methods from the location manager delegate
+/** @name Pass-through methods from the location manager delegate */
+/** Refer to the documentation for the CLLocationManagerDelegate protocol
+ 
+ @param manager the CLLocationManager
+ @param locations the locations given by the system
+ */
 - (void)locationManager:(CLLocationManager *)manager didUpdateLocations:(NSArray *)locations;
-/// @name Pass-through methods from the location manager delegate
+/** Refer to the documentation for the CLLocationManagerDelegate protocol
+ 
+ @param manager the CLLocationManager
+ @param error the error given by the system
+ */
 - (void)locationManager:(CLLocationManager *)manager didFailWithError:(NSError *)error;
 
 @end
 
+/** This class enables transactional manipulation of the application's segment subscriptions
+ 
+ */
 @interface GLESegmentTransaction : NSObject
-    
+/** @name Starting a transaction */
 /** Create a segment-manipulation transaction
  
  @return a new segment-manipulation transaction object
  @param push the GLECentral instance to manipulate
- @name Starting a transaction
  */
 -(instancetype)initWithGLECentral:(GLECentral *)push;
 
+/** @name Fundamental tag manipulation */
 /** Install (or replace) a single segment subscription
  
  This replaces any existing segment with the same name
  
  @param segment the segment object
- @name Fundamental tag manipulation
  */
 -(void)setSegmentSubscription:(GLESegment *)segment;
 /** Retrieve a single segment subscription
  
- @param name the application-defined segment name 
+ @param segmentName the application-defined segment name
  @return the segment object
- @name Fundamental tag manipulation
  */
 -(GLESegment *)segmentSubscriptionForName:(NSString *)segmentName;
 /** Install (or replace) all segments
  
  Any existing segments are removed and replaced by the contents of the segments array
  
- @param segments the application-defined segment subscriptions
- @name Fundamental tag manipulation
+ @param segments the application-defined segment subscriptions as an array of GLESegment objects
  */
 -(void)setSegmentSubscriptions:(NSArray *)segments;
 /** Retrieve all segment subscriptions
  
- @return an array of segment objects representing all the segment subscriptions
- @name Fundamental tag manipulation
+ @return an array of GLESegment objects representing all the segment subscriptions
  */
 -(NSArray *)segmentSubscriptions;
-    
+
+/** @name Easy tag manipulation */
 /** Ensure a tag is set within the given segment
  
  If the tag is not set, this adds it; if it is set, it does nothing.
  
  @param tag the application-defined tag name
  @param segmentName the application-defined segment name
- @name Easy tag manipulation
  */
 - (void)addTag:(NSString *)tag toSegment:(NSString *)segmentName;
 /** Ensure a tag is not set within the given segment
@@ -161,30 +210,25 @@ Iff the first call is to sharedGrooveLionWithServerURL the server URL of the sha
  
  @param tag the application-defined tag name
  @param segmentName the application-defined segment name
- @name Easy tag manipulation
  */
 - (void)removeTag:(NSString *)tag fromSegment:(NSString *)segmentName;
 /** Ensure there is exactly one tag set within the given segment
  
  @param tag the application-defined tag name
  @param segmentName the application-defined segment name
- @name Easy tag manipulation
  */
 - (void)setSingleTag:(NSString *)tag forSegment:(NSString *)segmentName;
-    
+
+/** @name Ending a transaction */
 /** Discard the changes made in this transaction
  
  Any further changes after this point are a new transaction which can be committed or discarded independently.
- 
- @name Ending a transaction
- */
+  */
 -(void)discardChanges;
 
 /** Commit the changes made in this transaction
  
  Any further changes after this point are a new transaction which can be committed or discarded independently.
- 
- @name Ending a transaction
  */
 -(void)commitChanges;
     
